c4efd9d8aeac96eb078dd460c641127dbf1c9099
Sam <530566495@qq.com>
2016-07-11 16:26:18 +0800

    初始化第一版
    第一个版本，解决最基本需求

---
+++ EChartsNet/EchartData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EChartsNet
+{
+    /************************************************************************************ 
+     * Copyright (c) 2016  All Rights Reserved. 
+     * CLR版本： 4.0.30319.34014 
+     * 机器名称：CP 
+     * 公司名称： 
+     * 命名空间：EChartsNet 
+     * 文件名：  EchartData 
+     * 版本号：  V1.0.0.0 
+     * 创建人：  caipeng
+     * 电子邮箱：530566495@qq.com 
+     * 创建时间：2016-07-11 16:01:43 
+     * 描述    :
+     * =====================================================================
+     * 修改时间：2016-07-11 16:01:43 
+     * 修改人  ：  
+     * 版本号  ： V1.0.0.0 
+     * 描述    ：
+    */
+    /// <summary>
+    /// EchartData 的摘要说明
+    /// </summary>
+    public class EchartData
+    {
+        public List<string> legend = new List<string>();//数据分组
+        public List<string> category = new List<string>();//横坐标
+        public List<Series> series = new List<Series>();//纵坐标 
+
+        public EchartData(List<string> legendList, List<string> categoryList, List<Series> seriesList)
+        {
+            this.legend = legendList;
+            this.category = categoryList;
+            this.series = seriesList;
+        }
+    }
+
+}
+
+
+
---
+++ EChartsNet/Series.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EChartsNet
+{
+    /************************************************************************************ 
+   * Copyright (c) 2016  All Rights Reserved. 
+   * CLR版本： 4.0.30319.34014 
+   * 机器名称：CP 
+   * 公司名称： 
+   * 命名空间：EChartsNet 
+   * 文件名：  Series 
+   * 版本号：  V1.0.0.0 
+   * 创建人：  caipeng
+   * 电子邮箱：530566495@qq.com 
+   * 创建时间：2016-07-11 16:01:43 
+   * 描述    :
+   * =====================================================================
+   * 修改时间：2016-07-11 16:01:43 
+   * 修改人  ：  
+   * 版本号  ： V1.0.0.0 
+   * 描述    ：
+  */
+    public class Series
+    {
+        public String name;
+
+        public String type;
+
+        public List<int> data;//这里要用int 不能用String 不然前台显示不正常（特别是在做数学运算的时候）  
+
+        public Series(String name, String type, List<int> data)
+        {
+            this.name = name;
+            this.type = type;
+            this.data = data;
+        }
+    }
+}
